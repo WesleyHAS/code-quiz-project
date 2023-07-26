d03bfc9079a5ff9219d186590065a6b5bdc3f548
Wes <wesley.has@outlook.com>
2023-07-25 19:44:14 -0600

    added second question and transition

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,7 @@
 var secondsLeft = 60;
 var startGameScreen = document.getElementById('start-game-screen');
 var screen1 = document.getElementById('quiz-screen-1');
+var screen2 = document.getElementById('quiz-screen-2');
 
 function startGame() {
  var timerInterval = setInterval(function() {
@@ -19,3 +20,14 @@ function startGame() {
 
 var startQuizButton = document.getElementById('start-quiz');
 startQuizButton.addEventListener('click', startGame);
+
+var startNextQuestion = document.querySelectorAll('.questions-container .alternatives');
+
+for (var i = 0; i < startNextQuestion.length; i++) {
+  startNextQuestion[i].addEventListener('click', nextQuestion);
+}
+
+function nextQuestion() {
+  screen1.classList.add('hidden');
+  screen2.classList.remove('hidden');
+}
